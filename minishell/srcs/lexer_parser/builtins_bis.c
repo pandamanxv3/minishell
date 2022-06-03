d6a51c12d6965c0635d08bfe281f1086b2dad083
Camille BARBIT <cbarbit@e3r5p17.clusters.42paris.fr>
2022-06-03 20:54:45 +0200

    lol

--- minishell/srcs/lexer_parser/builtins_bis.c
+++ minishell/srcs/lexer_parser/builtins_bis.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 15:37:24 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/06/03 17:48:02 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/06/03 20:53:26 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,7 +106,6 @@ void	ft_chdir(char *path) // PWD ne marche pas dans un dossier suprimer 2/06/202
 	return_chdir = chdir(path); //j'essaie de changer de directory 
 	if (return_chdir == -1)
 	{
-		if (val_strncmp(".."))
 		if (open(path, O_RDONLY) == -1)
 		{
 			printf("cd: no such file or directory: %s\n", path);
