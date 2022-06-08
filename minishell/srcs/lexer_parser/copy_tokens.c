28e940a7db692462382d8e4d1eb8939baed988e2
Camille BARBIT <cbarbit@e3r6p15.clusters.42paris.fr>
2022-06-08 21:36:02 +0200

    STARTED NORMINETTE

--- minishell/srcs/lexer_parser/copy_tokens.c
+++ minishell/srcs/lexer_parser/copy_tokens.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   copy_tokens.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aboudjel <aboudjel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 16:04:32 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/06/06 17:53:10 by aboudjel         ###   ########.fr       */
+/*   Updated: 2022/06/08 21:33:55 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,99 +14,14 @@
 
 extern t_minishell	g_shell;
 
-// void	val_strlcpy(char *dst, char *src, int size)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (i < size)
-// 	{
-// 		dst[i] = src[i];
-// 		i++;
-// 	}
-// 	return ;
-// }
-
-// static int copy_var(char *copy, int i, char *str)
-// {
-// 	int		j;
-// 	int		k;
-// 	t_env	*temp;
-	
-// 	g_shell.tab_proc[i].index++;
-// 	if (str[g_shell.tab_proc[i].index] && (ft_isalpha(str[g_shell.tab_proc[i].index]) == 1 
-// 		|| str[g_shell.tab_proc[i].index] == '_' || str[g_shell.tab_proc[i].index] == '?'))
-// 		j = g_shell.tab_proc[i].index;
-// 	else
-// 	{
-// 		copy[0] = '$';
-// 		return (1);		
-// 	}
-// 	if (str[g_shell.tab_proc[i].index++] == '?')
-// 		return (itoa_remixed(copy));
-// 	while (str[g_shell.tab_proc[i].index] && (ft_isalnum(str[g_shell.tab_proc[i].index]) == 1 || str[g_shell.tab_proc[i].index] == '_'))
-// 		g_shell.tab_proc[i].index++;
-// 	temp = g_shell.lst_env;
-// 	while (temp)
-// 	{
-// 		k = val_strncmp((str + j), temp->var, (g_shell.tab_proc[i].index - j));
-
-// 		if (k == 0)
-// 		{
-// 			k = ft_strlen(temp->val);
-// 			val_strlcpy(copy, temp->val, k);
-// 			return (k);
-// 		}
-// 		temp = temp->next;
-// 	}
-// 	return (0);
-// }
-
-// static int copy_var(char *copy, int i, char *str)
-// {
-// 	int		j;
-// 	int		k;
-// 	t_env	*temp;
-	
-// 	g_shell.tab_proc[i].index++;
-// 	if (str[g_shell.tab_proc[i].index] == '\0')
-// 	{
-// 		copy[0] = '$';
-// 		return (1);		
-// 	}
-// 	j = g_shell.tab_proc[i].index;
-// 	if (str[g_shell.tab_proc[i].index++] == '?')
-// 		return (itoa_remixed(copy));
-// 	if (!str[g_shell.tab_proc[i].index + 1] && (ft_isalpha(str[g_shell.tab_proc[i].index]) == 1 && str[g_shell.tab_proc[i].index] != '_'))
-// 	{
-// 		copy[0] = '$';
-// 		return (1);
-// 	}
-// 	while (str[g_shell.tab_proc[i].index] && (ft_isalnum(str[g_shell.tab_proc[i].index]) == 1 || str[g_shell.tab_proc[i].index] == '_'))
-// 		g_shell.tab_proc[i].index++;
-// 	temp = g_shell.lst_env;
-// 	while (temp)
-// 	{
-// 		k = val_strncmp((str + j), temp->var, (g_shell.tab_proc[i].index - j));
-
-// 		if (k == 0)
-// 		{
-// 			k = ft_strlen(temp->val);
-// 			val_strlcpy(copy, temp->val, k);
-// 			return (k);
-// 		}
-// 		temp = temp->next;
-// 	}
-// 	return (0);
-// }
-
-
-static void copy_on_simple_quote(int	i, int j, char *str)
+static void	copy_on_simple_quote(int i, int j, char *str)
 {
 	g_shell.tab_proc[i].index++;
 	while (str[g_shell.tab_proc[i].index] != '\'')
 	{
-		g_shell.tab_proc[i].tab_token[j].word[g_shell.tab_proc[i].tab_token[j].word_malloc_count++] = str[g_shell.tab_proc[i].index++];
+		g_shell.tab_proc[i].tab_token[j].word
+		[g_shell.tab_proc[i].tab_token[j].word_malloc_count++]
+			= str[g_shell.tab_proc[i].index++];
 	}
 	g_shell.tab_proc[i].index++;
 }
@@ -115,7 +30,9 @@ static void	copy_on_double_quote(int i, int j, char *str)
 {
 	g_shell.tab_proc[i].index++;
 	while (str[g_shell.tab_proc[i].index] != '"')
-		g_shell.tab_proc[i].tab_token[j].word[g_shell.tab_proc[i].tab_token[j].word_malloc_count++] = str[g_shell.tab_proc[i].index++];
+		g_shell.tab_proc[i].tab_token[j].word
+		[g_shell.tab_proc[i].tab_token[j].word_malloc_count++]
+			= str[g_shell.tab_proc[i].index++];
 	g_shell.tab_proc[i].index++;
 }
 
@@ -124,25 +41,35 @@ void	copy_token(int i, int j, char *str)
 	g_shell.tab_proc[i].tab_token[j].word_malloc_count = 0;
 	while (str[g_shell.tab_proc[i].index] == ' ')
 		g_shell.tab_proc[i].index++;
-	if (str[g_shell.tab_proc[i].index] == '>' || str[g_shell.tab_proc[i].index] == '<')
+	if (str[g_shell.tab_proc[i].index] == '>'
+		|| str[g_shell.tab_proc[i].index] == '<')
 	{
-		while (str[g_shell.tab_proc[i].index] == '>' || str[g_shell.tab_proc[i].index] == '<')
-			g_shell.tab_proc[i].tab_token[j].word[g_shell.tab_proc[i].tab_token[j].word_malloc_count++] = str[g_shell.tab_proc[i].index++];
-		g_shell.tab_proc[i].tab_token[j].word[g_shell.tab_proc[i].tab_token[j].word_malloc_count] = '\0';
+		while (str[g_shell.tab_proc[i].index] == '>'
+			|| str[g_shell.tab_proc[i].index] == '<')
+			g_shell.tab_proc[i].tab_token[j].word
+			[g_shell.tab_proc[i].tab_token[j].word_malloc_count++]
+				= str[g_shell.tab_proc[i].index++];
+		g_shell.tab_proc[i].tab_token[j].word
+		[g_shell.tab_proc[i].tab_token[j].word_malloc_count]
+			= '\0';
 		return ;
 	}
-	while(str[g_shell.tab_proc[i].index] && str[g_shell.tab_proc[i].index] != ' '
-		&& str[g_shell.tab_proc[i].index] != '<' && str[g_shell.tab_proc[i].index] != '>')
+	while (str[g_shell.tab_proc[i].index]
+		&& str[g_shell.tab_proc[i].index] != ' '
+		&& str[g_shell.tab_proc[i].index] != '<'
+		&& str[g_shell.tab_proc[i].index] != '>')
 	{
 		if (str[g_shell.tab_proc[i].index] == '"')
-            copy_on_double_quote(i, j, str);
+			copy_on_double_quote(i, j, str);
 		else if (str[g_shell.tab_proc[i].index] == '\'')
-            copy_on_simple_quote(i, j, str);
+			copy_on_simple_quote(i, j, str);
 		else
-            g_shell.tab_proc[i].tab_token[j].word[g_shell.tab_proc[i].tab_token[j].word_malloc_count++] = str[g_shell.tab_proc[i].index++];		
+			g_shell.tab_proc[i].tab_token[j].word
+			[g_shell.tab_proc[i].tab_token[j].word_malloc_count++]
+				= str[g_shell.tab_proc[i].index++];
 	}
-	g_shell.tab_proc[i].tab_token[j].word[g_shell.tab_proc[i].tab_token[j].word_malloc_count] = '\0';
-//	printf(" count %d\n", g_shell.tab_proc[i].tab_token[j].word_malloc_count);
-//	printf("STRING3 %s\n", g_shell.tab_proc[i].tab_token[j].word);
+	g_shell.tab_proc[i].tab_token[j].word
+	[g_shell.tab_proc[i].tab_token[j].word_malloc_count]
+		= '\0';
 	return ;
 }
