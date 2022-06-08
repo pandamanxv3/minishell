88c58eacbc132af9f22698ff3e7df87f1d833d3c
Camille BARBIT <cbarbit@e3r6p15.clusters.42paris.fr>
2022-06-08 22:15:37 +0200

    STARTED NORMINETTE

--- minishell/srcs/lexer_parser/mallocs.c
+++ minishell/srcs/lexer_parser/mallocs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mallocs.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aboudjel <aboudjel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 16:44:28 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/06/06 18:20:36 by aboudjel         ###   ########.fr       */
+/*   Updated: 2022/06/08 22:15:24 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,24 @@ extern t_minishell	g_shell;
 
 int	malloc_processes(void)
 {
-	g_shell.tab_proc = malloc(sizeof(t_process) * (g_shell.nb_proc + 1)); //necessary the +1?
+	g_shell.tab_proc = malloc(sizeof(t_process)
+			* (g_shell.nb_proc + 1));
 	if (!g_shell.tab_proc)
 		mallocreturn_failed(g_shell.gc, g_shell.gc2, "t_process malloc failed");
 	else
-		ft_gcadd_back(g_shell.gc2, ft_gcnew(g_shell.tab_proc,g_shell.gc2));
+		ft_gcadd_back(g_shell.gc2, ft_gcnew(g_shell.tab_proc, g_shell.gc2));
 	return (0);
 }
 
 int	malloc_tokens(int i, int j)
 {
-	g_shell.tab_proc[i].tab_token[j].word = ft_malloc("str", g_shell.tab_proc[i].tab_token[j].word_malloc_count," word token malloc failed", g_shell.gc2);
-	return (0);	
+	g_shell.tab_proc[i].tab_token[j].word
+		= ft_malloc("str", g_shell.tab_proc[i].tab_token[j].word_malloc_count,
+			" word token malloc failed", g_shell.gc2);
+	return (0);
 }
 
-static void	on_simple_quote(int	i, int j, char *str)
+static void	on_simple_quote(int i, int j, char *str)
 {
 	g_shell.tab_proc[i].index++;
 	while (str[g_shell.tab_proc[i].index] != '\'')
@@ -57,15 +60,19 @@ void	size_malloc_tokens(int i, int j, char *str)
 	g_shell.tab_proc[i].tab_token[j].word_malloc_count = 0;
 	while (str[g_shell.tab_proc[i].index] == ' ')
 		g_shell.tab_proc[i].index++;
-	while (str[g_shell.tab_proc[i].index] == '>' || str[g_shell.tab_proc[i].index] == '<')
+	while (str[g_shell.tab_proc[i].index] == '>'
+		|| str[g_shell.tab_proc[i].index] == '<')
 	{
 		g_shell.tab_proc[i].tab_token[j].word_malloc_count++;
 		g_shell.tab_proc[i].index++;
-		if (str[g_shell.tab_proc[i].index] != '>' && str[g_shell.tab_proc[i].index] != '<')
-			return;
+		if (str[g_shell.tab_proc[i].index] != '>'
+			&& str[g_shell.tab_proc[i].index] != '<')
+			return ;
 	}
-	while(str[g_shell.tab_proc[i].index] && str[g_shell.tab_proc[i].index] != ' '
-		&& str[g_shell.tab_proc[i].index] != '<' && str[g_shell.tab_proc[i].index] != '>')
+	while (str[g_shell.tab_proc[i].index]
+		&& str[g_shell.tab_proc[i].index] != ' '
+		&& str[g_shell.tab_proc[i].index] != '<'
+		&& str[g_shell.tab_proc[i].index] != '>')
 	{
 		if (str[g_shell.tab_proc[i].index] == '"')
 			on_double_quote(i, j, str);
