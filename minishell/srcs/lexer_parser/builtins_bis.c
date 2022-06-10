53d99678fbbac4b11019bfea4568300398acbd91
Camille BARBIT <cbarbit@e3r7p17.42.fr>
2022-06-10 18:44:38 +0200

    NORMINETTE OK

--- minishell/srcs/lexer_parser/builtins_bis.c
+++ minishell/srcs/lexer_parser/builtins_bis.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins_bis.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aboudjel <aboudjel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 15:37:24 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/06/09 17:46:46 by aboudjel         ###   ########.fr       */
+/*   Updated: 2022/06/10 18:42:45 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,48 +28,32 @@ void	ft_pwd(void)
 	ft_gcadd_back(g_shell.gc, ft_gcnew(g_shell.pwd, g_shell.gc));
 }
 
-static void	read_and_replace_env_cd(char *var, char *val)
-{
-	t_env	*current;
-
-	current = g_shell.lst_env;
-	while (current)
-	{
-		if (val_strncmp(var, current->var, ft_strlen(var)) == 0)
-			current->val = val;
-		current = current->next;
-	}
-}
-
-static char	*get_home_val(void)
-{
-	t_env	*current;
-	char	*path;
-
-	current = g_shell.lst_env;
-	path = NULL;
-	while (current)
-	{
-		if (val_strncmp("HOME", current->var, 4) == 0)
-			path = current->val;
-		current = current->next;
-	}
-	return (path);
-}
-
-void	ft_chdir(char *path)
+static void	check_unset_pwd_status(void)
 {
 	char	*old_pwd;
-	char	*tempo;
-	int		return_chdir;
 
-	if (path == NULL)
-		path = get_home_val();
-	if (g_shell.unset_pwd != 1 && g_shell.unset_pwd != 2)
+	if (g_shell.unset_pwd == 1)
 	{
 		ft_pwd();
-		tempo = g_shell.pwd;
+		g_shell.temp_old_dir = g_shell.pwd;
+		unset("OLDPWD");
+		g_shell.unset_pwd = 2;
+		return ;
 	}
+	if (g_shell.unset_pwd == 2)
+	{
+		ft_export("OLDPWD=");
+		old_pwd = g_shell.temp_old_dir;
+		read_and_replace_env_cd("OLDPWD", old_pwd);
+		g_shell.unset_pwd = 0;
+		return ;
+	}	
+}
+
+static void	test_return_chdir(char *path)
+{
+	int	return_chdir;
+
 	if (path)
 		return_chdir = chdir(path);
 	if (!path || return_chdir == -1)
@@ -89,21 +73,24 @@ void	ft_chdir(char *path)
 		printf("cd: not a directory: %s\n", path);
 		g_shell.error = 1;
 		return ;
-	}
-	if (g_shell.unset_pwd == 1)
+	}	
+}
+
+void	ft_chdir(char *path)
+{
+	char	*tempo;
+
+	if (path == NULL)
+		path = get_home_val();
+	if (g_shell.unset_pwd != 1 && g_shell.unset_pwd != 2)
 	{
 		ft_pwd();
-		g_shell.temp_old_dir = g_shell.pwd;
-		unset("OLDPWD");
-		g_shell.unset_pwd = 2;
-		return ;
+		tempo = g_shell.pwd;
 	}
-	if (g_shell.unset_pwd == 2)
+	test_return_chdir(path);
+	if (g_shell.unset_pwd == 1 || g_shell.unset_pwd == 2)
 	{
-		ft_export("OLDPWD=");
-		old_pwd = g_shell.temp_old_dir;
-		read_and_replace_env_cd("OLDPWD", old_pwd);
-		g_shell.unset_pwd = 0;
+		check_unset_pwd_status();
 		return ;
 	}
 	ft_pwd();
